a009e2d65fca1970dea55130fa9c7d5e746ebce7
baicorp <bagusatoki@gmail.com>
2025-07-08 00:02:26 +0700

    add new prefix variant to decide content type

--- src/utils/contentType.ts
+++ src/utils/contentType.ts
@@ -2,7 +2,11 @@ export function contentType(id: string) {
   if (!id) return null;
   if (id?.startsWith("MPREb_")) {
     return "album";
-  } else if (id?.startsWith("VLRDCLAK5uy_") || id?.startsWith("VLPL")) {
+  } else if (
+    id?.startsWith("VLRDCLAK5uy_") ||
+    id?.startsWith("VLPL") ||
+    id?.startsWith("VLOL")
+  ) {
     return "playlist";
   } else if (id?.startsWith("UC") && id?.length === 24) {
     return "artist";
