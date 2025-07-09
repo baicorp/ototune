be79a946dfdb9668cac7fd77da2e239c6bcab4b2
baicorp <bagusatoki@gmail.com>
2025-07-09 13:07:03 +0700

    add new variant for contentType detector

--- src/utils/contentType.ts
+++ src/utils/contentType.ts
@@ -5,7 +5,8 @@ export function contentType(id: string) {
   } else if (
     id?.startsWith("VLRDCLAK5uy_") ||
     id?.startsWith("VLPL") ||
-    id?.startsWith("VLOL")
+    id?.startsWith("VLOL") ||
+    id?.startsWith("VLRD")
   ) {
     return "playlist";
   } else if (id?.startsWith("UC") && id?.length === 24) {
