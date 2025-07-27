f6adebdd2a939991997a6d85488917ae5fdf7e9d
baicorp <bagusatoki@gmail.com>
2025-07-27 14:20:33 +0700

    refactor leftPanel

--- src/components/RootLayout/LeftPanel.tsx
+++ src/components/RootLayout/LeftPanel.tsx
@@ -1,37 +1,30 @@
 import { NavLink } from "react-router";
-import { useRef, useState } from "react";
-
-function navClassName({ isActive }: { isActive: boolean }) {
-  return `flex gap-4 items-center ${isActive && "rounded-md bg-themed-card"}`;
-}
+import { FormEvent, useRef, useState } from "react";
 
 export default function LeftPanel() {
   return (
-    <nav className="md:w-52 lg:w-64 xl:w-72 sticky top-0 p-2 lg:p-3 border-r border-themed-border flex flex-col shrink-0">
-      <NavLink className={navClassName} to="/">
-        <div className="w-13 p-3 shrink-0 aspect-square rounded-md overflow-hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            className="fill-current"
-          >
-            <path d="M160-120v-480l320-240 320 240v480H560v-280H400v280H160Z" />
-          </svg>
-        </div>
-        <span className="hidden md:block">Home</span>
-      </NavLink>
-      <NavLink className={navClassName} to="/explore">
-        <div className="w-13 p-3 shrink-0 aspect-square rounded-md overflow-hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            className="fill-current"
-          >
-            <path d="m300-300 280-80 80-280-280 80-80 280Zm180-120q-25 0-42.5-17.5T420-480q0-25 17.5-42.5T480-540q25 0 42.5 17.5T540-480q0 25-17.5 42.5T480-420Zm0 340q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
-          </svg>
-        </div>
-        <span className="hidden md:block">Explore</span>
-      </NavLink>
+    <nav className="md:max-w-52 lg:max-w-64 xl:max-w-72 sticky top-0 p-2 lg:p-3 border-r border-themed-border flex flex-col shrink-0">
+      <NavigationLink href="/" title="Home">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -960 960 960"
+          className="fill-current p-3"
+        >
+          <path d="M160-120v-480l320-240 320 240v480H560v-280H400v280H160Z" />
+        </svg>
+      </NavigationLink>
+      <NavigationLink href="/explore" title="Explore">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 -960 960 960"
+          className="fill-current p-3"
+        >
+          <path d="m300-300 280-80 80-280-280 80-80 280Zm180-120q-25 0-42.5-17.5T420-480q0-25 17.5-42.5T480-540q25 0 42.5 17.5T540-480q0 25-17.5 42.5T480-420Zm0 340q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
+        </svg>
+      </NavigationLink>
+      <NavigationLink href="/experiment" title="Experiment">
+        🔮
+      </NavigationLink>
       <Library />
     </nav>
   );
@@ -39,14 +32,56 @@ export default function LeftPanel() {
 
 function Library() {
   return (
-    <>
-      <div className="flex justify-between items-center my-2 md:border-y md:border-themed-border">
-        <p className="hidden md:block cursor-default text-themed-text-muted">
-          Your library
-        </p>
-        <CreatePlaylist />
+    <div className="flex flex-col gap-2">
+      <CreatePlaylistButton />
+    </div>
+  );
+}
+
+function NavigationLink({
+  href,
+  title,
+  thumbnail,
+  description,
+  children,
+}: {
+  href: string;
+  title: string;
+  thumbnail?: string;
+  description?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <NavLink
+      className={({ isActive }) => {
+        return `flex gap-4 items-center ${isActive && "rounded-md bg-themed-card"}`;
+      }}
+      to={href}
+    >
+      <div className="w-13 shrink-0 aspect-square rounded-md overflow-hidden flex justify-center items-center">
+        {children}
+        {thumbnail && <img src={thumbnail} />}
       </div>
-    </>
+      <div className="hidden md:block">
+        <span className="line-clamp-1 mb-1">{title}</span>
+        {description && (
+          <span className="text-themed-text-muted line-clamp-1">
+            {description}
+          </span>
+        )}
+      </div>
+    </NavLink>
+  );
+}
+
+function CreatePlaylistButton() {
+  return (
+    <div className="flex justify-between items-center mt-2 border-t border-t-themed-border">
+      <p className="hidden md:block cursor-default text-themed-text-muted pl-3">
+        Your library
+      </p>
+      <CreatePlaylist />
+    </div>
   );
 }
 
@@ -62,7 +97,7 @@ function CreatePlaylist() {
     <>
       <button
         onClick={handleClick}
-        className="w-13 p-3 shrink-0 aspect-square rounded-md overflow-hidden bg-themed-card md:bg-transparent outline-0"
+        className="w-13 p-3 shrink-0 aspect-square rounded-md overflow-hidden hover:bg-themed-card outline-0"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -123,7 +158,7 @@ function CreatePlaylist() {
             <button
               disabled={load}
               type="submit"
-              className="disabled:bg-white/50 flex justify-center items-center px-3 py-1.5 rounded-full bg-white text-black"
+              className="disabled:bg-white/50 flex justify-center items-center px-3 py-1.5 rounded-full bg-white text-black font-semibold"
             >
               <span>Create</span>
               {load && (
