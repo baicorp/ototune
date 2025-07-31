bb3b1439442549c8c09211b5bf12d86eb722f5a6
baicorp <bagusatoki@gmail.com>
2025-07-31 13:06:58 +0700

    persist window app size across sessions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -622,6 +622,7 @@ async fn get_audio_url(app: tauri::AppHandle, video_id: String) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
